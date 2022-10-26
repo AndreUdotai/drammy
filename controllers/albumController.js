d2c85228021d745666ccea42b3e550468dd2bc4f
Ubong Udotai <31491736+AndreUdotai@users.noreply.github.com>
2022-10-26 02:57:36 +0100

    updated album controller to display all albums

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -1,21 +1,35 @@
 const { nextTick } = require("async");
 const Album = require("../models/album");
+const async = require("async");
+const Song = require("../models/song");
 
 // Display list of all Albums.
 exports.album_list = (req, res) => {
-  Album.find({}, 'name')
-    .sort({ name: 1 })
-    .populate('artist')
-    .exec(function (err, list_albums) {
+  async.parallel(
+    {
+      album(callback) {
+        Album.find({}, 'name')
+          .exec(callback);
+      },
+      album_songs(callback) {
+        Song.find({}, 'album artist' )
+          .populate('album')
+          .populate('artist')
+          .exec(callback);
+      },
+    },
+    (err, results) => {
       if (err) {
         return next(err);
       }
       // Successful, so render
       res.render('album_list', {
-        title: 'Album List',
-        album_list: list_albums,
+        title: "Album List",
+        album_list: results.album,
+        album_songs_list: results.album_songs,
       });
-    });
+    }
+  );
 };
 
 // Display detail page for a specific Album.
@@ -52,3 +66,15 @@ exports.album_update_get = (req, res) => {
 exports.album_update_post = (req, res) => {
   res.send("NOT IMPLEMENTED: Album update POST");
 };
+
+
+
+
+
+         //   - albums.push(album.name)
+          //  li
+               // a(href=album.url) #{album.name}
+        // - console.log(albums)
+        
+        //else
+        //li There are no albums
